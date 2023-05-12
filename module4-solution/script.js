5095be88e359bd4866834166bb0f7f26d76c1493
Siddami <dharmmy621@gmail.com>
2023-05-12 12:39:42 +0100

    module 4 solution js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -3,14 +3,12 @@
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
     for (var i = 0; i < names.length; i++) {
-        
-        if(names[i].charAt(0) === 'J' ){
             var firstLetter = names[i].charAt(0).toLowerCase();
             if (firstLetter === 'j') {
                 byeSpeaker(names[i]);
               } else {
                 helloSpeaker(names[i]);
               }
-        }
+        
     }
     })();
